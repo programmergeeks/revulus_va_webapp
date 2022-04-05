49c5dab3501510b89344fd970d628f0a371f674d
programmergeeks <aljecera.jamesalwin@gmail.com>
2022-04-05 23:44:47 +0800

    -Code changes on Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,32 @@
 import { Link } from 'react-scroll'
 function Navbar() {
+
+    const scrollUp = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth',
+        });
+    };
     return (
-        <nav className="navbar navbar-expand-sm bg-light justify-content-center">
-            <ul className="navbar-nav">
-                <li className='nav-item'>
-                    <Link className='nav-link' duration={100} activeClass="active"  to="home" spy={true} smooth={true}>Home</Link>
-                </li>
-                <li>
-                    <Link className='nav-link' duration={100} to="about" spy={true} smooth={true}>About</Link>
-                </li>
-                <li>
-                    <Link className='nav-link' duration={100} to="team" spy={true} smooth={true}>Team</Link>
-                </li>
-                <li>
-                    <Link className='nav-link' duration={100} to="contact_us" spy={true} smooth={true}>Contact</Link>
-                </li>
-            </ul>
-        </nav>          
+        <>
+            <nav className="navbar navbar-expand-sm justify-content-center">
+                <a className='navbar-brand justify-self-start' onClick={scrollUp}>Revulus VA</a>
+                <ul className="navbar-nav">
+                    <li className='nav-item'>
+                        <Link className='nav-link' duration={100} activeClass="active" to="home" spy={true} smooth={true}>Home</Link>
+                    </li>
+                    <li>
+                        <Link className='nav-link' duration={100} to="about" spy={true} smooth={true}>About</Link>
+                    </li>
+                    <li>
+                        <Link className='nav-link' duration={100} to="team" spy={true} smooth={true}>Team</Link>
+                    </li>
+                    <li>
+                        <Link className='nav-link' duration={100} to="contact_us" spy={true} smooth={true}>Contact</Link>
+                    </li>
+                </ul>
+            </nav>
+        </>
     );
 }
 export default Navbar;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 
 export default function Home() {
   return (
-    <h1>Home</h1>
+    <h1 style={{}}>Home</h1>
   )
 }
