597d4a0ee512e9a5a1c5c5120d4981539a52bc10
programmergeeks <ljecera.jamesalwin@gmail.com>
2022-04-27 14:03:24 -0800

    nth commit - added About Us Title

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,7 +3,7 @@ import '../css/about.css';
 export default function About() {
   return (
     <div id='about' className='row d-flex align-items-center justify-content-start'>
-      
+      <h2>About Us</h2>
     </div>
   )
 }
