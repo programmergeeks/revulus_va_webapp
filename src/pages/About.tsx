4bee811f9468563c155df8922591d34990aa0dc5
programmergeeks <ljecera.jamesalwin@gmail.com>
2022-04-28 13:50:06 -0800

    nth commit - About Us Page and CSS

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,8 +2,13 @@ import '../css/about.css';
 
 export default function About() {
   return (
-    <div id='about' className='row d-flex align-items-center justify-content-start'>
-      <h2>About Us</h2>
+    <div  id='about' className='row d-flex'>
+      <div className='col'>
+        <h1 className='text-center'>Revulus VA - Our Cyber Solution for Appointment Setters</h1>
+      </div>
+      <div className='col'>
+
+      </div>
     </div>
   )
 }
